430953390c3004cfb52507e889df7e1e7177423d
JacobBrown <jcb99mt@byui.edu>
2025-03-21 14:41:56 -0600

    pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("How to Build a Streamlit App: DS 460")
+st.subheader(
+    "Steps to building a new Streamlit app for DS 460:")
+
+st.write("Step 1: Navigate tot the Streamlit Website:  https://streamlit.io/")
+st.write("Step 2: Sign up for a free account using your github account")
+st.write("Step 3: Click on the Get Started red button")
+st.write("Step 4: Then click on the build in Public with Sreamlit community cloud")
+st.write("Step 5: Then select your Template, (for class we are going to select the 'Blank app' template)")
+st.write("Step 6: Now its time to start coding your app 🤙")
+st.write("Step 7: go to the Coding page of this websigt to see what type of code you can try out 👈")
+
